9b7df3a590386dab5a940a83819fc65ce0261c2a
Kissmybulletsdf <142477901+Kissmybulletsdf@users.noreply.github.com>
2023-08-19 15:16:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,8 +21,8 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://graph.org/file/6a2506bdf72802406b9a2.mp4")
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Kissmybulletsdf/mission")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/guardians_Bot_Updates")
